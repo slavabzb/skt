f975f1beab30f38ee53e23a2c7d58f8a190c9448
Vyacheslav Bezborodov <vyacheslav.bezborodov@gmail.com>
2016-09-29 22:15:12 +0500

    add cryptoapi

---
+++ lab3/CryptoAPI/CryptoAPI/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CryptoAPI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Location = new System.Drawing.Point(43, 108);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSource.TabIndex = 2;
+            // 
+            // comboBoxType
+            // 
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Items.AddRange(new object[] {
+            "Symmetric",
+            "Asymmetric",
+            "Hashing"});
+            this.comboBoxType.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxType.TabIndex = 4;
+            this.comboBoxType.Text = "Select a type";
+            this.comboBoxType.SelectedIndexChanged += new System.EventHandler(this.comboBoxType_SelectedIndexChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(52, 166);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Text";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(53, 143);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Origin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Result";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.comboBoxType);
+            this.Controls.Add(this.textBoxSource);
+            this.Name = "MainForm";
+            this.Text = "CryptoAPI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.ComboBox comboBoxType;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab3/CryptoAPI/CryptoAPI/MainForm.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CryptoAPI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBoxType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.label.Text = "";
+            this.labelResult.Text = "";
+
+            string original = this.textBoxSource.Text;
+
+            try
+            {
+                if (this.comboBoxType.SelectedIndex == 0)
+                {
+                    // Symmetric
+
+                    // Create a new instance of the AesManaged class.
+                    // This generates a new key and initialization vector (IV).
+                    using (AesManaged myAes = new AesManaged())
+                    {
+                        // Encrypt the string to an array of bytes.
+                        byte[] encrypted = Aes.EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+
+                        this.labelResult.Text = Convert.ToBase64String(encrypted);
+
+                        // Decrypt the bytes to a string.
+                        string roundtrip = Aes.DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+
+                        //Display the original data and the decrypted data.
+                        this.label.Text = roundtrip;
+                    }
+                }
+                else if (this.comboBoxType.SelectedIndex == 1)
+                {
+                    // Asymmetric
+
+                    //lets take a new CSP with a new 2048 bit rsa key pair
+                    var csp = new RSACryptoServiceProvider(2048);
+
+                    //how to get the private key
+                    var privKey = csp.ExportParameters(true);
+
+                    //and the public key ...
+                    var pubKey = csp.ExportParameters(false);
+
+                    //we have a public key ... let's get a new csp and load that key
+                    csp = new RSACryptoServiceProvider();
+                    csp.ImportParameters(pubKey);
+
+                    //for encryption, always handle bytes...
+                    var bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(original);
+
+                    //apply pkcs#1.5 padding and encrypt our data 
+                    var bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+
+                    //we might want a string representation of our cypher text... base64 will do
+                    var cypherText = Convert.ToBase64String(bytesCypherText);
+
+                    this.labelResult.Text = cypherText;
+
+                    //first, get our bytes back from the base64 string ...
+                    bytesCypherText = Convert.FromBase64String(cypherText);
+
+                    //we want to decrypt, therefore we need a csp and load our private key
+                    csp = new RSACryptoServiceProvider();
+                    csp.ImportParameters(privKey);
+
+                    //decrypt and strip pkcs#1.5 padding
+                    bytesPlainTextData = csp.Decrypt(bytesCypherText, false);
+
+                    //get our original plainText back...
+                    this.label.Text = System.Text.Encoding.Unicode.GetString(bytesPlainTextData);
+                }
+                else if (this.comboBoxType.SelectedIndex == 2)
+                {
+                    // Hashing
+
+                    byte[] bytes = Encoding.UTF8.GetBytes(original);
+
+                    // Initialize a SHA256 hash object.
+                    SHA256 mySHA256 = SHA256Managed.Create();
+                    byte[] hash = mySHA256.ComputeHash(bytes);
+                    
+                    string hashString = string.Empty;
+                    foreach (byte x in hash)
+                    {
+                        hashString += String.Format("{0:x2}", x);
+                    }
+
+                    this.labelResult.Text = hashString;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                this.label.Text = ex.Message;
+            }
+        }
+    }
+
+
+    class Aes
+    {
+        public static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Create an AesManaged object
+            // with the specified key and IV.
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+
+        }
+
+        public static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an AesManaged object
+            // with the specified key and IV.
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+        }
+    }
+}
