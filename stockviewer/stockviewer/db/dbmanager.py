adc40e9ccc13d76ab4aab0a1d40c3048cd733746
Vyacheslav Bezborodov <v.bezborodov@pressfoto.ru>
2016-10-03 14:35:41 +0500

    move make_migration to dbmanager

--- stockviewer/stockviewer/db/dbmanager.py
+++ stockviewer/stockviewer/db/dbmanager.py
@@ -1,4 +1,6 @@
+import os
 import logging
+import settings
 
 from provider import provider_factory
 
@@ -9,17 +11,50 @@ class dbmanager():
 		factory = provider_factory(self.__config.find('provider_factory'))		
 		self.__provider = factory.create_provider(self.__config.find('provider').text)
 
-	def apply(self, sql_script):
-		try:
-			self.__provider.apply(sql_script)
-		except Exception as e:
-			logging.warning(e)
-			raise e
+	def make_migration(self):
+		current_version = 0
 
-	def execute(self, query):
-		logging.info(query)
 		try:
-			return self.__provider.execute(query)
-		except Exception as e:
-			logging.warning(e)
-			raise e
+			c = self.__provider.execute('select version, dirty from MIGRATION_VERSION')
+			res = c.fetchone()
+			current_version = int(res[0])
+			dirty = int(res[1])
+			if dirty:
+				logging.error('Migration {} is broken. Please, fix it manually. Aborting.'.format(current_version))
+				return
+		except:
+			pass
+
+		logging.info('Current database version is {}'.format(current_version))
+
+		for dirpath, dirnames, filenames in os.walk(settings.dirs['migration']):
+			for new_version in sorted(dirnames):
+				files = []
+
+				if int(new_version) == current_version:
+					try:
+						c = self.__provider.execute('select * from MIGRATION_FILES')
+						files = c.fetchall()
+					except:
+						pass
+
+				if int(new_version) > current_version:
+					try:
+						self.__provider.execute('delete from MIGRATION_FILES')
+					except:
+						pass
+
+				try:
+					if new_version >= current_version:
+						for filename in sorted(os.listdir(os.path.join(dirpath, new_version))):
+							if (unicode(filename),) in files:
+								logging.info('Skipping file `{}`'.format(filename))
+							else:
+								logging.info('Applying file `{}`'.format(filename))
+								with open(os.path.join(dirpath, new_version, filename), 'rb') as f:
+									self.__provider.apply(f.read())
+									self.__provider.execute('insert into MIGRATION_FILES values (\'{}\')'.format(filename))
+						self.__provider.execute('update MIGRATION_VERSION set `version` = {}'.format(int(new_version)))
+				except Exception as e:
+					self.__provider.execute('update MIGRATION_VERSION set `dirty` = 1')
+					raise e
--- stockviewer/stockviewer/service/service.py
+++ stockviewer/stockviewer/service/service.py
@@ -1,6 +1,4 @@
-import os
 import logging
-import settings
 
 from db import dbmanager
 
@@ -23,56 +21,7 @@ class service():
 		logging.info('Service started')
 
 		if self.__args.make_migration:
-			self.__make_migration()
+			self.__db.make_migration()
 
 	def stop(self):
 		logging.info('Service stopped')
-
-	def __make_migration(self):
-		current_version = 0
-		
-		try:
-			c = self.__db.execute('select version, dirty from MIGRATION_VERSION')
-			res = c.fetchone()
-			current_version = int(res[0])
-			dirty = int(res[1])
-			if dirty:
-				logging.error('Migration {} is broken. Please, fix it manually. Aborting.'.format(current_version))
-				return
-		except:
-			pass
-
-		logging.info('Current database version is {}'.format(current_version))
-
-		for dirpath, dirnames, filenames in os.walk(settings.dirs['migration']):
-			for new_version in sorted(dirnames):
-				files = []
-
-				if int(new_version) == current_version:
-					try:
-						c = self.__db.execute('select * from MIGRATION_FILES')
-						files = c.fetchall()
-					except:
-						pass
-
-				if int(new_version) > current_version:
-					try:
-						self.__db.execute('delete from MIGRATION_FILES')
-					except:
-						pass
-
-				try:
-					if new_version >= current_version:
-						for filename in sorted(os.listdir(os.path.join(dirpath, new_version))):
-							if (unicode(filename),) in files:
-								logging.info('Skipping file `{}`'.format(filename))
-							else:
-								logging.info('Applying file `{}`'.format(filename))
-								with open(os.path.join(dirpath, new_version, filename), 'rb') as f:
-									self.__db.apply(f.read())
-									self.__db.execute('insert into MIGRATION_FILES values (\'{}\')'.format(filename))
-						self.__db.execute('update MIGRATION_VERSION set `version` = {}'.format(int(new_version)))
-				except Exception as e:
-					print e
-					self.__db.execute('update MIGRATION_VERSION set `dirty` = 1')
-					return
